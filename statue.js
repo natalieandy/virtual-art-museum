c4efc58a7d06d1e2d97e012dcc2c64d7f8fb1a2c
Natalie Anderson <natalie.anderson6@gmail.com>
2020-11-05 20:15:50 -0800

    Added text, the lady vector, and positioned images

--- statue.js
+++ statue.js
@@ -1,5 +1,5 @@
 function setStatueFrame (n) {
-  let statueContainer = document.querySelector('.sprite2');
-  let offset = -558 * n
+  let statueContainer = document.querySelector('.statue');
+  let offset = -484 * n
   statueContainer.style['background-position'] = `${offset}px 0px`;
 }
